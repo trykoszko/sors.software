663f6bb4336ecf61996b66c03be3625a4ebbe855
Michal Trykoszko <trykoszkom@gmail.com>
2022-12-17 15:16:44 +0100

    update top bar color

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>sors.software - bespoke software development</title>
         <meta name="description" content="Software developers experienced in working with biggest global brands" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#9089FC" />
+        <meta name="theme-color" content="#14191c" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
